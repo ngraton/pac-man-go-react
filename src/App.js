a0bd5e1e01e8ec51f1cd43da2c8f358a467a32be
ngraton <ngraton@gmail.com>
2019-07-25 13:58:37 -0500

    depolyment for early testing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import OpenLayersMap from './components/OpenLayersMap';
+
+function App() {
+  return (
+    <div>
+      <p>Hello World</p>
+      <OpenLayersMap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/OpenLayersMap.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import Feature from 'ol/Feature.js';
+import Geolocation from 'ol/Geolocation.js';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import Point from 'ol/geom/Point.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import {OSM, Vector as VectorSource} from 'ol/source.js';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style.js';
+
+class OpenLayersMap extends React.Component {
+
+  componentDidMount() {
+    // eslint-disable-next-line
+    var view = new View({
+      center: [0, 0],
+      zoom: 2
+    });
+
+    var map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      target: 'map',
+      view: view
+    });
+
+    var geolocation = new Geolocation({
+      // enableHighAccuracy must be set to true to have the heading value.
+      trackingOptions: {
+        enableHighAccuracy: true
+      },
+      projection: view.getProjection()
+    });
+
+    function el(id) {
+      return document.getElementById(id);
+    }
+
+    el('track').addEventListener('change', function() {
+      geolocation.setTracking(this.checked);
+    });
+
+    // update the HTML page when the position changes.
+    geolocation.on('change', function() {
+      el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+      el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+      el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+      el('heading').innerText = geolocation.getHeading() + ' [rad]';
+      el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+    });
+
+    // handle geolocation error.
+    geolocation.on('error', function(error) {
+      var info = document.getElementById('info');
+      info.innerHTML = error.message;
+      info.style.display = '';
+    });
+
+    var accuracyFeature = new Feature();
+    geolocation.on('change:accuracyGeometry', function() {
+      accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+    });
+
+    var positionFeature = new Feature();
+    positionFeature.setStyle(new Style({
+      image: new CircleStyle({
+        radius: 6,
+        fill: new Fill({
+          color: '#3399CC'
+        }),
+        stroke: new Stroke({
+          color: '#fff',
+          width: 2
+        })
+      })
+    }));
+
+    geolocation.on('change:position', function() {
+      var coordinates = geolocation.getPosition();
+      positionFeature.setGeometry(coordinates ?
+        new Point(coordinates) : null);
+    });
+
+    new VectorLayer({
+      map: map,
+      source: new VectorSource({
+        features: [accuracyFeature, positionFeature]
+      })
+    });
+  }
+  render() {
+    return (
+    <div>
+      <div id="map" className="map"></div>
+      <div id="info" ></div>
+      <label htmlFor="track">
+        track position
+        <input id="track" type="checkbox"/>
+      </label>
+      <p>
+        position accuracy : <code id="accuracy"></code>&nbsp;&nbsp;
+        altitude : <code id="altitude"></code>&nbsp;&nbsp;
+        altitude accuracy : <code id="altitudeAccuracy"></code>&nbsp;&nbsp;
+        heading : <code id="heading"></code>&nbsp;&nbsp;
+        speed : <code id="speed"></code>
+      </p>
+    </div>
+    )
+  }
+}
+
+export default OpenLayersMap;
